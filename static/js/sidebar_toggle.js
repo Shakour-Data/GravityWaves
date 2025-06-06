076b6c657e7d4fe30c438b02598004832627f17d
GravityWavesOrg <gravitywaves2000@gmail.com>
2025-06-06 17:45:59 +0330

    New UI

--- static/js/sidebar_toggle.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const sidebarToggle = document.getElementById('sidebar-toggle');
-    const sidebar = document.getElementById('sidebar');
-
-    sidebarToggle.addEventListener('click', () => {
-        const isCollapsed = sidebar.classList.toggle('collapsed');
-        sidebarToggle.setAttribute('aria-expanded', !isCollapsed);
-    });
-});
